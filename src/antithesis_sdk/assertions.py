8581268516e21258a26c161d74648d2bd171269c
Paul Herzog <paul.herzog@antithesis.com>
2024-11-08 16:30:09 -0500

    Repair line formatting

--- src/antithesis_sdk/assertions.py
+++ src/antithesis_sdk/assertions.py
@@ -487,7 +487,6 @@ _CATALOG = os.getenv(ASSERTION_CATALOG_ENV_VAR)
 if _CATALOG is not None:
     cat_path = Path(_CATALOG)
     if cat_path.is_dir():
-
         instrumentation_folder = _get_instrumentation_folder(_CATALOG)
         if instrumentation_folder is not None:
             instrumentation_path = os.path.join(_CATALOG, instrumentation_folder)
